7a233418e22b5fffd18d038bad2b48dcba1645b8
iilinegor <iilinegor@gmail.com>
2016-11-06 06:12:55 +0700

    Fix indexes, image upload, default route, double 'mark' handle

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect, IndexRoute } from 'react-router';
 
 import All from './components/All.jsx';
 import Main from './components/Main.jsx';
@@ -15,9 +15,9 @@ import './App.css';
 ReactDOM.render(
 			<Router history={hashHistory}>					
 				
-				<Route path="/" component={App}>
+				<Route path="/" component={App} >
 					<Route path="/all" component={All}/>
-					<Route path="/main" component={Main}/>
+					<IndexRoute component={Main}/>
 					<Route path="/add" component={Add_start}/>
 					<Route path="/host" component={Host}/>
 					<Route path="/profile/:id" component={Profile}/>
